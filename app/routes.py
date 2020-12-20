2b7fa8b8108fa8596155d8a3e0bea91a1c89568f
Alex Khomenko <web.dev.alex72@gmail.com>
2020-12-20 23:34:43 +0200

    Add some bootstrap changes

--- app/routes.py
+++ app/routes.py
@@ -51,9 +51,9 @@ def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('index', page=posts.next_num) \
+    next_url = url_for('explore', page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('index', page=posts.prev_num) \
+    prev_url = url_for('explore', page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('index.html', title='Explore', posts=posts.items,
                             next_url=next_url, prev_url=prev_url)
@@ -94,7 +94,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login-bs'))
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
--- config.py
+++ config.py
@@ -7,11 +7,11 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BOOTSTRAP_BOOTSWATCH_THEME = 'sketchy'
+    BOOTSTRAP_BOOTSWATCH_THEME = 'lumen'
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['web.dev.alex72@gmail.com']
-    POSTS_PER_PAGE = 25
+    POSTS_PER_PAGE = 3
