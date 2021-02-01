132ec6ead71e5a42128085c197ce57e455110735
Alex Khomenko <web.dev.alex72@gmail.com>
2021-02-01 09:44:22 +0200

    remove changing for herku

--- config.py
+++ config.py
@@ -28,5 +28,4 @@ class Config(object):
     POSTS_PER_PAGE = 25
     LANGUAGES = ['en', 'ru']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
-    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')  
